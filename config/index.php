615c4af86f38383f609295448b2c25cae6390250
James <james.noble@digitalskillsacademy.me>
2018-10-01 23:07:00 +0200

    Init scripts done
    Database table and data initialisation scripts done. Has been tested on AWS. Next to add query as JSON

--- config/index.php
+++ config/index.php
@@ -11,9 +11,10 @@
 File list:
 <ul>
     <li><strong>config/db.class.php</strong><p>Database connection class</p></li>
-    <li><strong><a href="config/create.php">config/create.php</a></strong><p>Init/reinitialise the database tables</p></li>
+    <li><strong>config/init_db.php</strong><p>Init/reinitialise the database tables</p></li>
+    <li><strong>config/init_data.php</strong><p>Initialise table data</p></li>
     <li><strong><a href="config/testdb.php">config/testdb.php</a></strong><p>Use to check the AWS configuration</p></li>
-    <li><strong><a href="config/testlocaldb.php">config/testlocaldb.php</a></strong><p>Use to test the local database</p></li>
+    <li><strong>config/testlocaldb.php</strong><p>Use to test the local database</p></li>
     <li><strong>index.php</strong><p>this page</p></li>
 </ul>
 </body>
